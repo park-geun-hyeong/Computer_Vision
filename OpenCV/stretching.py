5b0b3ee95076e7a9290fdab42f95b37fe93c6a9b
park <park30908@gmail.com>
2022-04-05 10:51:59 +0900

    normalize & equalization

--- OpenCV/stretching.py
+++ OpenCV/stretching.py
@@ -11,11 +11,24 @@ path = './data_0405/'
 if __name__ == "__main__":
     src = cv2.imread(path + 'lec10_Hawkes.jpg', cv2.IMREAD_GRAYSCALE)
     dst = cv2.normalize(src, None, 0, 255, cv2.NORM_MINMAX)
+    dst_eq = cv2.equalizeHist(src)
+
+    fig, ax = plt.subplots(1, 2, figsize=(12, 4))
+
     hist_src = cv2.calcHist([src], [0], None, [256], [0,256])
     hist_dst = cv2.calcHist([dst], [0], None, [256], [0, 256])
-    plt.plot(hist_src, 'r', label = 'src')
-    plt.plot(hist_dst, 'b', label = 'dst')
-    plt.legend()
+    hist_eq = cv2.calcHist([dst_eq], [0], None, [256], [0, 256])
+    ax[0].plot(hist_src, 'r', label = 'src')
+    ax[0].plot(hist_dst, 'b', label = 'dst')
+    ax[0].legend()
+    ax[0].set_title('normalize')
+
+    ax[1].plot(hist_src, 'r', label='src')
+    ax[1].plot(hist_eq, 'g', label='dst')
+    ax[1].set_title('equalization')
+    ax[1].legend()
+
     cv2.imshow('dst', dst)
     cv2.imshow('src', src)
+    cv2.imshow('dst_eq', dst_eq)
     plt.show()
